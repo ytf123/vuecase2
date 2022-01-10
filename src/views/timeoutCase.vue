584dcade7526e9bf7dc94cb139d35e5424367fbe
ytf123 <1287014821@qq.com>
2022-01-10 10:34:49 +0800

    配置

---
+++ src/views/timeoutCase.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div id="counter">
+      Counter: {{counter}}
+      <button @click="addCounter">addCounter</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    let counter = ref(0);
+
+    const addCounter = () => {
+      this.counter++;
+    }
+  }
+})
+</script>
