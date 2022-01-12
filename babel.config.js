d5426a82a51670fd29cf5a706ef5296b48b0990f
ytf123 <1287014821@qq.com>
2022-01-12 16:58:49 +0800

    Revert "peizhi 2"
    This reverts commit aacd74e6501259e4a837b6d83d85b4d8e178d765.

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-    // '@vue/app'
-  ],
-  plugins: [
-    [
-      'import',
-      { libraryName: 'ant-design-vue', libraryDirectory: 'es', style: true }
-    ]
   ]
 }
